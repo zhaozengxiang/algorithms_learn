d23d1fd0eaf29fe0efa213b8cc893cc742bbcf6e
zhaozengxiang <zhao4660178@gmail.com>
2021-03-14 17:25:43 +0800

    创建项目

---
+++ src/homework/week1/TwoSum.java
@@ -0,0 +1,26 @@
+package homework.week1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums,target)));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        if (nums == null) {
+            return null;
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
